22271dd019e84406b012b08e2918dde2cb528014
Manuel <manuel_marx@outlook.de>
2025-01-16 12:24:59 +0100

    3. Iteration / output() / Typumwandlung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,7 +5,8 @@ public class App {
     public static void main(String[] args) {
 
         /*
-         *  Instanziierung --> Erzeugen eines Objekts
+         *  Instanziierung --> 
+         * Erzeugen eines Objekts durch Aufruf eines Konstruktors
          *  Objekt <---> Instanz
          *  cat --> Referenzvariable
          *  Cat --> Referenztyp(Klasse) 
@@ -13,11 +14,19 @@ public class App {
          */
 
         Cat cat = new Cat();
-        System.out.println(cat);
-        
+        output("Blick von aussen: " + cat);
 
+        output("---------------------");
+
+        Cat cat2 = new Cat();
+        output("Blick von aussen: " + cat2);
+        
+        // output("hi");
     }
 
+    public static void output(String outputStr){
+        System.out.println(outputStr);
+    }
    
 
 }
