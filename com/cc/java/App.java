99f47cc68bc6c1e89de24bad4bd46fa63cbeab2e
Manuel <manuel_marx@outlook.de>
2025-01-16 11:08:25 +0100

    Klasse / Objekt Instanziierung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,17 @@ public class App {
     
     public static void main(String[] args) {
 
+        /*
+         *  Instanziierung --> Erzeugen eines Objekts
+         *  Objekt <---> Instanz
+         *  cat --> Referenzvariable
+         *  Cat --> Referenztyp(Klasse) 
+         * 
+         */
+
+        Cat cat = new Cat();
+        System.out.println(cat);
+        
 
     }
 
